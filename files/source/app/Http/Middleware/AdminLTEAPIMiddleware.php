e7e35a165dcece361b43675a30439525af34799e
Aykut Aydınlı <aykut@pyronome.com>
2020-05-04 21:26:51 +0300

    API middleware updated

--- files/source/app/Http/Middleware/AdminLTEAPIMiddleware.php
+++ files/source/app/Http/Middleware/AdminLTEAPIMiddleware.php
@@ -5,11 +5,10 @@ namespace App\Http\Middleware;
 use Closure;
 use App\AdminLTE;
 use App\AdminLTEUser;
-use App\HTMLDB;
 
 /* {{snippet:begin_class}} */
 
-class AdminLTEHTMLDBMiddleware
+class AdminLTEAPIMiddleware
 {
 
 	/* {{snippet:begin_properties}} */
