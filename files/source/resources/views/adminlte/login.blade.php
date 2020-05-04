986e3251e6dacae8d5f217b46d1e47f1e8e4eeb2
Aykut Aydınlı <aykut@pyronome.com>
2020-05-04 19:38:36 +0300

    Views updated

--- files/source/resources/views/adminlte/login.blade.php
+++ files/source/resources/views/adminlte/login.blade.php
@@ -1,6 +1,8 @@
 @include('adminlte.head')
 <body class="hold-transition login-page" data-url-prefix=""  data-page-url="login">
-    <login-form></login-form>
+    <div id="app">
+        <login-form></login-form>
+    </div>
     <script src="/js/adminlte/app.js"></script>
     <script src="/js/adminlte/login.js"></script>
 </body>
