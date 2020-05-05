7d1d4049d280946abe34815e6c6aa0b168640fcd
Aykut Aydınlı <aykut@pyronome.com>
2020-05-05 13:04:14 +0300

    ForgotPasswordPOSTRequest class updated

--- files/source/app/Http/Requests/AdminLTE/API/ForgotPasswordPOSTRequest.php
+++ files/source/app/Http/Requests/AdminLTE/API/ForgotPasswordPOSTRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\AdminLTE\API;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\AdminLTE;
+use App\AdminLTEUser;
 
 class ForgotPasswordPOSTRequest extends FormRequest
 {
