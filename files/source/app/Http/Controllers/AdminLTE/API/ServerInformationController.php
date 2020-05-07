0999d8b0e7370a006c44d8978de0f8877758a973
Aykut Aydınlı <aykut@pyronome.com>
2020-05-07 19:07:56 +0300

    Files updated

--- files/source/app/Http/Controllers/AdminLTE/API/ServerInformationController.php
+++ files/source/app/Http/Controllers/AdminLTE/API/ServerInformationController.php
@@ -9,30 +9,9 @@ use App\AdminLTEUser;
 
 class ServerInformationController extends Controller
 {
-    public $columns = [
-		'id',
-		'OS_header',
-		'OS_detail',
-		'OS_icon_src',
-		'WEB_header',
-		'WEB_detail',
-		'WEB_icon_src',
-		'APP_header',
-		'APP_detail',
-		'APP_icon_src',
-		'DB_header',
-		'DB_detail',
-		'DB_icon_src'
-    ];
-
-    public $protectedColumns = [];
-    public $row = [];
-
     public function get(Request $request)
     {
-
-		$list = array();
-		$list[0]['id'] = 1;
+		$data = array();
 
 		// Server OS
 		$OS_detail = php_uname();
@@ -56,9 +35,9 @@ class ServerInformationController extends Controller
 			break;
 		} // switch (PHP_OS) {
 
-		$list[0]['OS_header'] = $OS_header;
-		$list[0]['OS_detail'] = $OS_detail;
-		$list[0]['OS_icon_src'] = $OS_icon_src;
+		$data['OS_header'] = $OS_header;
+		$data['OS_detail'] = $OS_detail;
+		$data['OS_icon_src'] = $OS_icon_src;
 
 		// Web Server
 		$WEB_detail = $_SERVER['SERVER_SOFTWARE'];
@@ -73,14 +52,14 @@ class ServerInformationController extends Controller
 			$WEB_header = 'Nginx Web Server';
 		} // if (strpos(strtolower($WEB_detail), 'apache') !== false) {
 
-		$list[0]['WEB_header'] = $WEB_header;
-		$list[0]['WEB_detail'] = $WEB_detail;
-		$list[0]['WEB_icon_src'] = $WEB_icon_src;
+		$data['WEB_header'] = $WEB_header;
+		$data['WEB_detail'] = $WEB_detail;
+		$data['WEB_icon_src'] = $WEB_icon_src;
 
 		// Application
-		$list[0]['APP_header'] = 'PHP';
-		$list[0]['APP_detail'] = 'PHP ' . PHP_VERSION;
-		$list[0]['APP_icon_src'] = 'php.png';
+		$data['APP_header'] = 'PHP';
+		$data['APP_detail'] = 'PHP ' . PHP_VERSION;
+		$data['APP_icon_src'] = 'php.png';
 
 		$DB_icon_src = 'mysql.png';
 		$DB_header = 'MySQL';
@@ -93,17 +72,11 @@ class ServerInformationController extends Controller
 				. $DB_host);
 		*/
 
-		$list[0]['DB_header'] = $DB_header;
-		$list[0]['DB_detail'] = $DB_detail;
-		$list[0]['DB_icon_src'] = $DB_icon_src;
+		$data['DB_header'] = $DB_header;
+		$data['DB_detail'] = $DB_detail;
+		$data['DB_icon_src'] = $DB_icon_src;
 
-        $objectHTMLDB = new HTMLDB();
-		$objectHTMLDB->list = $list;
-        $objectHTMLDB->columns = $this->columns;
-        $objectHTMLDB->printHTMLDBList();
-
-        return;
-
+        return $data;
     }
 
 }
--- files/source/resources/js/adminlte/components/ServerInformationPage.vue
+++ files/source/resources/js/adminlte/components/ServerInformationPage.vue
@@ -10,14 +10,14 @@
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="/{{ AdminLTEHelper.getMainFolder }}/home">{{ $t('Home') }}</a></li>
+                            <li class="breadcrumb-item"><a href="#">{{ $t('Home') }}</a></li>
                             <li class="breadcrumb-item enabled">{{ $t('Server Information') }}</li>
                         </ol>
                     </div>
                 </div>
             </div>
         </div>
-        <section class="content htmldb-section" data-htmldb-table="ServerInformationHTMLDB">
+        <section class="content">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-xs-12">
@@ -32,11 +32,11 @@
                                             <div class="card-body pt-0">
                                                 <div class="row">
                                                     <div class="col-7">
-                                                        <h2 class="lead"><b>{{OS_header}}</b></h2>
-                                                        <p class="text-muted text-sm">{{OS_detail}}</p>
+                                                        <h2 class="lead"><b v-bind="OS_header"></b></h2>
+                                                        <p class="text-muted text-sm" v-bind="OS_detail"></p>
                                                     </div>
                                                     <div class="col-5 text-right">
-                                                        <img data-htmldb-content="/assets/adminlte/img/{{OS_icon_src}}" src="" alt="" class="img-fluid">
+                                                        <img src="" alt="" class="img-fluid">
                                                     </div>
                                                 </div>
                                             </div>
@@ -50,11 +50,11 @@
                                             <div class="card-body pt-0">
                                                 <div class="row">
                                                     <div class="col-7">
-                                                        <h2 class="lead"><b>{{WEB_header}}</b></h2>
-                                                        <p class="text-muted text-sm">{{WEB_detail}}</p>
+                                                        <h2 class="lead"><b v-bind="WEB_header"></b></h2>
+                                                        <p class="text-muted text-sm" v-bind="WEB_detail"></p>
                                                     </div>
                                                     <div class="col-5 text-right">
-                                                        <img data-htmldb-content="/assets/adminlte/img/{{WEB_icon_src}}" src="" alt="" class="img-fluid">
+                                                        <img src="" alt="" class="img-fluid">
                                                     </div>
                                                 </div>
                                             </div>
@@ -68,11 +68,11 @@
                                             <div class="card-body pt-0">
                                                 <div class="row">
                                                     <div class="col-7">
-                                                        <h2 class="lead"><b>{{APP_header}}</b></h2>
-                                                        <p class="text-muted text-sm">{{APP_detail}}</p>
+                                                        <h2 class="lead"><b v-bind="APP_header"></b></h2>
+                                                        <p class="text-muted text-sm" v-bind="APP_detail"></p>
                                                     </div>
                                                     <div class="col-5 text-right">
-                                                        <img data-htmldb-content="/assets/adminlte/img/{{APP_icon_src}}" src="" alt="" class="img-fluid">
+                                                        <img src="" alt="" class="img-fluid">
                                                     </div>
                                                 </div>
                                             </div>
@@ -86,11 +86,11 @@
                                             <div class="card-body pt-0">
                                                 <div class="row">
                                                     <div class="col-7">
-                                                        <h2 class="lead"><b>{{DB_header}}</b></h2>
-                                                        <p class="text-muted text-sm">{{DB_detail}}</p>
+                                                        <h2 class="lead"><b v-bind="DB_header"></b></h2>
+                                                        <p class="text-muted text-sm" v-bind="DB_detail"></p>
                                                     </div>
                                                     <div class="col-5 text-right">
-                                                        <img data-htmldb-content="/assets/adminlte/img/{{DB_icon_src}}" alt="" class="img-fluid">
+                                                        <img alt="" class="img-fluid">
                                                     </div>
                                                 </div>
                                             </div>
@@ -109,9 +109,16 @@
 <script>
 export default {
     data() {
-        return [
-
-        ];
+        return {
+            "OS_header": "",
+            "OS_detail": "",
+            "WEB_header": "",
+            "WEB_detail": "",
+            "APP_header": "",
+            "APP_detail": "",
+            "DB_header": "",
+            "DB_detail": ""
+        };
     },
     mounted() {
         AdminLTEHelper.initializeSideMenu();
