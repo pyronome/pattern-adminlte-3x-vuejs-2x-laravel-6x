6c1124c2486607371e813b07a81eb79aaa8612d9
Aykut Aydınlı <aykut@pyronome.com>
2020-05-09 15:12:16 +0300

    Select2 vue element added

--- files/source/resources/js/adminlte/adminltehelper.js
+++ files/source/resources/js/adminlte/adminltehelper.js
@@ -1,18 +1,6 @@
 var AdminLTEHelper = {
     "initializeApplication": function () {
 
-        Vue.directive('select', {
-        twoWay: true,
-        bind: function (el, binding, vnode) {
-            $(el).select2().on("select2:select", (e) => {
-            // v-model looks for
-            //  - an event named "change"
-            //  - a value with property path "$event.target.value"
-            el.dispatchEvent(new Event('change', { target: e.target }));
-            });
-        },
-        });
-
     },
     "initializeSideMenu": function () {
         $(".main-sidebar .active").removeClass("active");
---
+++ files/source/resources/js/adminlte/components/Select2Element.vue
@@ -0,0 +1,35 @@
+<template>
+    <select>
+        <slot></slot>
+    </select>
+</template>
+
+<script>
+export default {
+    props: ['options', 'value'],
+    mounted() {
+        var vm = this
+        $(this.$el)
+            .val(this.value)
+            // init select2
+            .select2({ data: this.options })
+            // emit event on change.
+            .on('change', function () {
+                vm.$emit('input', this.value)
+            });
+    },
+    watch: {
+        value: function (value) {
+            // update value
+            $(this.$el).val(value);
+        },
+        options: function (options) {
+            // update options
+            $(this.$el).select2({ data: options });
+        }
+    },
+    destroyed() {
+        $(this.$el).off().select2('destroy');
+    }
+}
+</script>
