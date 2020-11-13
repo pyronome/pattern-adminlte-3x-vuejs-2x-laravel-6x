e670e0653e21ce5340bc26c8a8ad0d1e43538667
hakan erdal <hakan.erdal@sanpark.com>
2020-11-13 18:49:26 +0300

    Removed css version named

--- files/source/resources/views/adminlte/head.blade.php
+++ files/source/resources/views/adminlte/head.blade.php
@@ -8,8 +8,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Tell the browser to be responsive to screen width -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script>document.write("<link rel='stylesheet' src='/css/adminlte/app.css?v=" + Date.now() + "'>");</script>
-        <script>document.write("<link rel='stylesheet' src='/css/adminlte/app.css?v=" + Date.now() + "'>");</script>
+        <link rel="stylesheet" href="/css/adminlte/app.css">
+        <link rel="stylesheet" href="/css/adminlte/custom.css">
         <script>var __publicAssetsURL = "{{ URL::asset('/') }}";</script>
         <script>var __storageURL = "{{ URL::asset('/storage/') }}/";</script>
     </head>
