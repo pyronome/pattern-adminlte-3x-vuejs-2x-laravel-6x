3366ec2309f0301165a78523a27e3139b52beb5e
Aykut Aydınlı <aykut@pyronome.com>
2020-11-23 19:28:02 +0300

    Profile detail page updated

--- files/source/resources/js/adminlte/components/pages/default/profile/detail.vue
+++ files/source/resources/js/adminlte/components/pages/default/profile/detail.vue
@@ -44,12 +44,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-lg-12 col-md-12 col-xs-12">
-                                        <div class="detail-container profile-image-container">
-                                            <div v-html="data.profile_img__displaytext__"></div>
-                                        </div>
-                                    </div>
-									<div class="col-lg-12 col-md-12 col-xs-12">
+									<div class="col-lg-6 col-md-6 col-xs-12">
                                         <div class="detail-container">
                                             <label class="detail-label">{{ $t('Fullname') }}</label>
                                             <div v-html="data.fullname__displaytext__"></div>
@@ -63,6 +58,11 @@
                                             <div v-html="data.email__displaytext__"></div>
                                         </div>
 									</div>
+                                    <div class="col-lg-6 col-md-6 col-xs-12">
+                                        <div class="detail-container profile-image-container">
+                                            <div v-html="data.profile_img__displaytext__"></div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
