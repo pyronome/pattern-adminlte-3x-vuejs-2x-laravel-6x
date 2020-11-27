e13cbcb9193ae9f4c260872946fabd4435baf0bf
hakan erdal <hakan.erdal@sanpark.com>
2020-11-27 11:17:49 +0300

    Updated Select2 processes

--- files/source/resources/js/adminlte/components/elements/default/Select2Element.vue
+++ files/source/resources/js/adminlte/components/elements/default/Select2Element.vue
@@ -7,31 +7,30 @@
 <script>
 export default {
     props: ['options', 'value', 'readonly'],
+    data() {
+        return {
+            initial_value: undefined,
+            value_initialized: false,
+            options_initialized: false,
+        }
+    },
     mounted() {
         var vm = this;
-        $(this.$el)
-            .select2({ data: this.options })
-            .val(this.value)
-            .trigger("change")
-            .on('change', function () {
-                vm.$emit('input', $(this).val())
-            });
+        $(this.$el).select2({ data: this.options });
     },
     watch: {
         value: function (value) {
-            if (!Array.isArray(value)) {
-                if (value !== $(this.$el).val()) {
-                    $(this.$el).val(value).trigger('change');
-                }
-            } else {
-                if ([...value].sort().join(",") !== [...$(this.$el).val()].sort().join(",")) {
-                    $(this.$el).val(value).trigger('change');
-                }
-            }
+            this.initial_value = value;
+            this.setValue(value);
         },
         options: function (options) {
             if (undefined !== options && 0 != options.length) {
                 $(this.$el).select2({ data: options });
+                this.options_initialized = true;
+
+                if ((!this.value_initialized) && (undefined !== this.initial_value)) {
+                    this.setValue(this.initial_value);
+                }
             }
         },
         readonly: function (readonly) {
@@ -39,6 +38,29 @@ export default {
                 $(this.$el).closest('div').find('.select2-container:first').addClass('select2-readonly')
             }
         },
+    },
+    methods: {
+        setValue: function(value) {
+            if (!this.options_initialized) {
+                return;
+            }
+
+            if (!Array.isArray(value)) {
+                if (value !== $(this.$el).val()) {
+                    $(this.$el).val(value).trigger('change').on('change', function () {
+                        this.$emit('input', $(this).val())
+                    });
+                }
+            } else {
+                if ([...value].sort().join(",") !== [...$(this.$el).val()].sort().join(",")) {
+                    $(this.$el).val(value).trigger('change').on('change', function () {
+                        this.$emit('input', $(this).val())
+                    });
+                }
+            }
+
+            this.value_initialized = true;
+        }
     },
     destroyed() {
         $(this.$el).off().select2('destroy');
