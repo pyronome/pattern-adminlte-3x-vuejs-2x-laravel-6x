5f49b002cec39c4e40b3e856421a1150a4a43e7b
Aykut Aydınlı <aykut@pyronome.com>
2020-12-05 20:05:33 +0300

    Update 0200.php

--- files/snippets/system/source/app/Providers/RouteServiceProvider.php/end_methods/0200.php
+++ files/snippets/system/source/app/Providers/RouteServiceProvider.php/end_methods/0200.php
@@ -9,13 +9,8 @@
     protected function mapAdminLTERoutes()
     {
         $adminLTEFolder = config('adminlte.main_folder');
-
-        /* {{@snippet:begin_adminlte_routes}} */
-
         Route::prefix($adminLTEFolder)
                 ->middleware(['web', AdminLTEMiddleware::class])
                 ->namespace('App\Http\Controllers\AdminLTE')
                 ->group(base_path('routes/adminlte.php'));
-
-        /* {{@snippet:end_adminlte_routes}} */
     }
