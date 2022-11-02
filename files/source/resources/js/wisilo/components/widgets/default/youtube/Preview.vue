7374f78f1414969f0edb8fd5cd7c3e7dcef729ef
Hakan <hakan.erdal@sanpark.com>
2022-11-02 14:54:46 +0300

    youtube preview videosu güncellendi

--- files/source/resources/js/wisilo/components/widgets/default/youtube/Preview.vue
+++ files/source/resources/js/wisilo/components/widgets/default/youtube/Preview.vue
@@ -2,7 +2,7 @@
     <div>
         <widget-preview-dialog :widget_name="widget_name">
             <iframe width="100%" height="315"
-                 src="https://www.youtube.com/embed/WafvIXtktMs" 
+                 src="https://www.youtube.com/embed/tkUaepjU-Yg" 
                  title="YouTube video player" 
                  frameborder="0" 
                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
